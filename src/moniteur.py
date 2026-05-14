be05f28ec5ee45082b691c9c49a3b895517f8278
aissatoubintoumohammadou <aissatou.bintou@saintjeaningenieur.org>
2026-05-14 20:25:57 +0100

    Ajout du moniteur.py

--- src/moniteur.py
+++ src/moniteur.py
@@ -4,9 +4,18 @@ class MoniteurReseau:
         self.paquets_transmis = 0
         self.paquets_perdus = 0
         self.historique = []
+  
+    def paquet_transmis(self):
+        self.paquets_transmis += 1
+  
+    def paquet_perdu(self):
+        self.paquets_perdus += 1
 
 moniteur = MoniteurReseau()
 
+moniteur.paquet_transmis()
+moniteur.paquet_transmis()
+moniteur.paquet_perdu()
+
 print(moniteur.paquets_transmis)
-print(moniteur.paquets_perdus)        
-print(moniteur.historique)
+print(moniteur.paquets_perdus)
