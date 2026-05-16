fb0e00e18a239fac6e29868c01bceef63208ed26
aissatoubintoumohammadou <aissatou.bintou@saintjeaningenieur.org>
2026-05-16 20:20:40 +0100

    Ajout  de la statistique réseau

--- src/moniteur.py
+++ src/moniteur.py
@@ -1,24 +1,37 @@
 class MoniteurReseau:
+
     def __init__(self):
-        
+
         self.paquets_transmis = 0
         self.paquets_perdus = 0
         self.historique = []
-  
+
     def paquet_transmis(self):
+
         self.paquets_transmis += 1
-  
+
     def paquet_perdu(self):
+
         self.paquets_perdus += 1
+
     def ajouter_paquet(self, paquet):
+
         self.historique.append(paquet)
+
         if len(self.historique) > 10:
+
             self.historique.pop(0)
+
+    def afficher_statistiques(self):
+
+        print(" STATISTIQUES RÉSEAU ")
+        print("Paquets transmis :", self.paquets_transmis)
+        print("Paquets perdus :", self.paquets_perdus)
+        print("Nombre de paquets dans l'historique :", len(self.historique))
 
 moniteur = MoniteurReseau()
 
-moniteur.ajouter_paquet("paquet 1")
-moniteur.ajouter_paquet("paquet 2")
-moniteur.ajouter_paquet("paquet 3")
-
-print(moniteur.historique)
+moniteur.paquet_transmis()
+moniteur.paquet_transmis()
+moniteur.paquet_perdu()
+moniteur.afficher_statistiques()
