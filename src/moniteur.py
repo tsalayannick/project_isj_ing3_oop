58436ccf35d5b712a14dfa223b9868ff1ec5da37
aissatoubintoumohammadou <aissatou.bintou@saintjeaningenieur.org>
2026-05-18 19:47:17 +0100

    Ajout liens et equipements

--- src/moniteur.py
+++ src/moniteur.py
@@ -5,6 +5,9 @@ class MoniteurReseau:
         self.paquets_transmis = 0
         self.paquets_perdus = 0
         self.historique = []
+        self.stats_liens = {}
+        self.equipements_actifs = []
+        self.equipements_inactifs = []
 
     def paquet_transmis(self):
 
@@ -22,16 +25,31 @@ class MoniteurReseau:
 
             self.historique.pop(0)
 
+    def enregistrer_lien(self, nom_lien, octets):
+
+        if nom_lien not in self.stats_liens:
+            self.stats_liens[nom_lien] = 0
+        self.stats_liens[nom_lien] += octets
+
+    def mettre_a_jour_equipements(self, liste_equipements):
+
+        self.equipements_actifs = [e for e in liste_equipements if e.statut == "actif"]
+        self.equipements_inactifs = [e for e in liste_equipements if e.statut == "inactif"]
+
     def afficher_statistiques(self):
 
         print(" STATISTIQUES RÉSEAU ")
         print("Paquets transmis :", self.paquets_transmis)
         print("Paquets perdus :", self.paquets_perdus)
         print("Nombre de paquets dans l'historique :", len(self.historique))
+        print("Équipements actifs :", len(self.equipements_actifs))
+        print("Équipements inactifs :", len(self.equipements_inactifs))
 
 moniteur = MoniteurReseau()
-
+moniteur.paquet_transmis()
 moniteur.paquet_transmis()
 moniteur.paquet_transmis()
 moniteur.paquet_perdu()
+moniteur.ajouter_paquet("paquet1")
+moniteur.ajouter_paquet("paquet2")
 moniteur.afficher_statistiques()
